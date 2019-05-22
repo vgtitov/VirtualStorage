e741265210988d623a3b5aac97036cdeec4b22a0
vgtitov <v.titov.7@gmail.com>
2019-05-22 16:14:12 +0300

    Initial Commit

---
+++ VirtualStorage/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VirtualStorage
+//
+//  Created by Admin on 22/05/2019.
+//  Copyright © 2019 Tsitou Vyachaslau. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
